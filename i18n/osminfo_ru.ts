5eae8b8020a489195e476b2f288c134081a54426
Maxim Dubinin <sim@gis-lab.info>
2015-12-26 18:40:12 -0600

    i18n

---
+++ i18n/osminfo_ru.ts
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="osminfo.py" line="69"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="osminfo.py" line="69"/>
+        <source>QGIS %s detected.
+</source>
+        <translation>Обнаружен QGIS %s
+</translation>
+    </message>
+    <message>
+        <location filename="osminfo.py" line="69"/>
+        <source>This version of OSMInfo requires at least QGIS version 2.0.
+Plugin will not be enabled.</source>
+        <translation>Эта версия OSMInfo требует QGIS минимум версии 2.0.
+Плагин работать не будет.</translation>
+    </message>
+    <message>
+        <location filename="osminfo.py" line="76"/>
+        <source>OSMInfo</source>
+        <translation>OSMInfo</translation>
+    </message>
+    <message>
+        <location filename="osminfo.py" line="76"/>
+        <source>Get OSM info for a point</source>
+        <translation>Получить информацию из OSM</translation>
+    </message>
+</context>
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="aboutdialog.py" line="56"/>
+        <source>Version: %s</source>
+        <translation>Версия: %s</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.py" line="81"/>
+        <source>&lt;p&gt;This plugin takes coordinates of a mouse click and extracts information for nearby and enclosing objects.&lt;/p&gt;&lt;p&gt;It uses Overpass API to extract data from OpenStreetMap.&lt;p&gt;&lt;strong&gt;Developers&lt;/strong&gt;: Maxim Dubinin and Alexander Lisovenko (&lt;a href=&quot;http://nextgis.com&quot;&gt;NextGIS&lt;/a&gt;).&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Homepage&lt;/strong&gt;: &lt;a href=&quot;https://github.com/nextgis/osminfo&quot;&gt;https://github.com/nextgis/osminfo&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Please report bugs at &lt;a href=&quot;https://github.com/nextgis/osminfo/issues&quot;&gt;bugtracker&lt;/a&gt;&lt;/p&gt;</source>
+        <translation>&lt;p&gt;Это расширение coordinates of a mouse click and extracts information for nearby and enclosing objects.&lt;/p&gt;&lt;p&gt;It uses Overpass API to extract data from OpenStreetMap.&lt;p&gt;&lt;strong&gt;Developers&lt;/strong&gt;: Maxim Dubinin and Alexander Lisovenko (&lt;a href=&quot;http://nextgis.com&quot;&gt;NextGIS&lt;/a&gt;).&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Homepage&lt;/strong&gt;: &lt;a href=&quot;https://github.com/nextgis/osminfo&quot;&gt;https://github.com/nextgis/osminfo&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Please report bugs at &lt;a href=&quot;https://github.com/nextgis/osminfo/issues&quot;&gt;bugtracker&lt;/a&gt;&lt;/p&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="14"/>
+        <source>About OSMInfo</source>
+        <translation>О OSMInfo</translation>
+    </message>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="44"/>
+        <source>TextLabel</source>
+        <translation>TextLabel</translation>
+    </message>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="30"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:16pt; font-weight:600;&quot;&gt;OSMInfo&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/aboutdialogbase.ui" line="67"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OSMInfo</name>
+    <message>
+        <location filename="osminfo.py" line="81"/>
+        <source>About OSMInfo...</source>
+        <translation>О OSMInfo</translation>
+    </message>
+</context>
+</TS>
