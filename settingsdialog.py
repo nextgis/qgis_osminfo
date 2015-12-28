acc879bb829ee727febea56148b26f46037ec2c6
Maxim Dubinin <sim@gis-lab.info>
2015-12-28 09:23:13 -0600

    settings dialog files

---
+++ settingsdialog.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+#******************************************************************************
+#
+# OSMInfo
+# ---------------------------------------------------------
+# This plugin takes coordinates of a mouse click and gets information about all 
+# objects from this point from OSM using Overpass API.
+#
+# Author:   Maxim Dubinin, sim@gis-lab.info
+# *****************************************************************************
+# Copyright (c) 2015. NextGIS, info@nextgis.com
+#
+# This source is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 2 of the License, or (at your option)
+# any later version.
+#
+# This code is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# A copy of the GNU General Public License is available on the World Wide Web
+# at <http://www.gnu.org/licenses/>. You can also obtain it by writing
+# to the Free Software Foundation, 51 Franklin Street, Suite 500 Boston,
+# MA 02110-1335 USA.
+#
+#******************************************************************************
+
+
+import os
+import ConfigParser
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+from ui.ui_settingsdialogbase import Ui_Dialog
+
+import resources
+
+
+class SettingsDialog(QDialog, Ui_Dialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        self.setupUi(self)
+
+        self.btnHelp = self.buttonBox.button(QDialogButtonBox.Help)
+
+        self.lblLogo.setPixmap(QPixmap(':/plugins/osminfo/icons/osminfo.png'))
+
+        cfg = ConfigParser.SafeConfigParser()
+        cfg.read(os.path.join(os.path.dirname(__file__), 'metadata.txt'))
+        version = cfg.get('general', 'version')
+        self.lblName.setText(self.tr('OSMInfo Settings'))
+
+        #self.lblVersion.setText(self.tr('Version: %s') % version)
+    
+        self.buttonBox.helpRequested.connect(self.openHelp)
+
+    def reject(self):
+        QDialog.reject(self)
+
+    def openHelp(self):
+        overrideLocale = QSettings().value('locale/overrideFlag', False, type=bool)
+        if not overrideLocale:
+            localeFullName = QLocale.system().name()
+        else:
+            localeFullName = QSettings().value('locale/userLocale', '')
+
+        localeShortName = localeFullName[0:2]
+        if localeShortName in ['ru', 'uk']:
+            QDesktopServices.openUrl(QUrl('http://gis-lab.info/qa/osminfo.html'))
+        else:
+            QDesktopServices.openUrl(QUrl('http://gis-lab.info/qa/qtiles-en.html'))
+
+    
