0bb041e72e91f9b8f43cf0bfe4733d222efc66ae
Maxim Dubinin <sim@gis-lab.info>
2016-01-03 17:18:13 -0600

    plugin settings

---
+++ plugin_settings.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ Common Plugins settings
+
+ NextGIS
+                             -------------------
+        begin                : 2014-10-31
+        git sha              : $Format:%H$
+        copyright            : (C) 2014 by NextGIS
+        email                : info@nextgis.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+from PyQt4.QtCore import QSettings
+
+
+class PluginSettings():
+
+    _company_name = 'NextGIS'
+    _product = 'OSMInfo'
+
+    @classmethod
+    def product_name(cls):
+        return cls._product
+
+    @classmethod
+    def get_settings(cls):
+        return QSettings(cls._company_name, cls._product)
+
+    @classmethod
+    def distance_value(cls):
+        return cls.get_settings().value('distance', 20, int)
+
+    @classmethod
+    def set_distance_value(cls, int_val):
+        cls.get_settings().setValue('distance', int_val)
