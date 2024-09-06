6001abb3db4e1c8cca9c14b35efbf5d65adfab7f
Ivan Barsukov <i.barsukov@protonmail.com>
2024-09-06 20:06:53 +0700

    Update translations

--- src/osminfo/i18n/osminfo_ru.ts
+++ src/osminfo/i18n/osminfo_ru.ts
@@ -2,6 +2,59 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru_RU">
 <context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../about_dialog.py" line="52"/>
+        <source> by NextGIS</source>
+        <translation> от NextGIS</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="170"/>
+        <source>REPORT_END</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="175"/>
+        <source>Developers</source>
+        <translation>Разработчики</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="176"/>
+        <source>Homepage</source>
+        <translation>Домашняя страница</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="177"/>
+        <source>Please report bugs at</source>
+        <translation>Пожалуйста, сообщайте о багах в</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="179"/>
+        <source>bugtracker</source>
+        <translation>багтрекер</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="180"/>
+        <source>Video with an overview of the plugin</source>
+        <translation>Видео с обзором на плагин</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="181"/>
+        <source>Other helpful services by NextGIS</source>
+        <translation>Другие полезные сервисы от NextGIS</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="182"/>
+        <source>Convenient up-to-date data extracts for any place in the world</source>
+        <translation>Удобная выборка актуальных данных из любой точки мира</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="185"/>
+        <source>Fully featured Web GIS service</source>
+        <translation>Полнофункциональный Веб ГИС-сервис</translation>
+    </message>
+</context>
+<context>
     <name>AboutDialogBase</name>
     <message>
         <location filename="../ui/about_dialog_base.ui" line="14"/>
@@ -9,56 +62,64 @@
         <translation>О {plugin_name}</translation>
     </message>
     <message>
-        <location filename="../ui/about_dialog_base.ui" line="27"/>
-        <source>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</source>
-        <translation>&lt;p align=&quot;center&quot;&gt;{plugin_name}&lt;/p&gt;</translation>
+        <location filename="../ui/about_dialog_base.ui" line="33"/>
+        <source>Information</source>
+        <translation>Информация</translation>
     </message>
     <message>
-        <location filename="../ui/about_dialog_base.ui" line="40"/>
-        <source>&lt;p&gt;{description}&lt;/p&gt;
-&lt;p&gt;{about}&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Developers:&lt;/b&gt; &lt;a href=&quot;{main_url}&quot;&gt;{authors}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Homepage:&lt;/b&gt; &lt;a href=&quot;{homepage_url}&quot;&gt;{homepage_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Video with an overview of the plugin:&lt;/b&gt; &lt;a href=&quot;{video_url}&quot;&gt;
-{video_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Please report bugs at&lt;/b&gt; &lt;a href=&quot;{tracker_url}&quot;&gt;bugtracker&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;Other helpful services by NextGIS:
-&lt;ul&gt;&lt;li&gt;&lt;b&gt;Convenient up-to-date data extracts for any place in the world:
-&lt;a href=&quot;{main_url}/{utm}&quot;&gt;{main_url}&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;
-&lt;li&gt;&lt;b&gt;Fully featured Web GIS service:
-&lt;a href=&quot;{main_url}/nextgis-com/plans{utm}&quot;&gt;
-{main_url}/nextgis-com/plans&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;&lt;/ul&gt;
-&lt;/p&gt;</source>
-        <translation>&lt;p&gt;{description}&lt;/p&gt;
-&lt;p&gt;{about}&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Разработчики:&lt;/b&gt; &lt;a href=&quot;{main_url}&quot;&gt;{authors}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Домашняя страница:&lt;/b&gt; &lt;a href=&quot;{homepage_url}&quot;&gt;{homepage_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Видео с обзором плагина:&lt;/b&gt; &lt;a href=&quot;{video_url}&quot;&gt;
-{video_url}&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;&lt;b&gt;Пожалуйста, сообщайте о багах в&lt;/b&gt; &lt;a href=&quot;{tracker_url}&quot;&gt;багтрекер&lt;/a&gt;&lt;/p&gt;
-&lt;p&gt;Другие полезные сервисы NextGIS:
-&lt;ul&gt;&lt;li&gt;&lt;b&gt;Удобная выборка актуальных данных из любой точки мира:
-&lt;a href=&quot;{main_url}/{utm}&quot;&gt;{main_url}&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;
-&lt;li&gt;&lt;b&gt;Полнофункциональный веб-ГИС-сервис:
-&lt;a href=&quot;{main_url}/nextgis-com/plans{utm}&quot;&gt;
-{main_url}/nextgis-com/plans&lt;/a&gt;&lt;/b&gt;&lt;/li&gt;&lt;/ul&gt;
-&lt;/p&gt;</translation>
+        <location filename="../ui/about_dialog_base.ui" line="54"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="72"/>
+        <source>License</source>
+        <translation>Лицензия</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="94"/>
+        <source>Components</source>
+        <translation>Компоненты</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="116"/>
+        <source>Contributors</source>
+        <translation>Участники</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="158"/>
+        <source>{plugin_name}</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="168"/>
+        <source>Version {version}</source>
+        <translation>Версия {version}</translation>
+    </message>
+    <message>
+        <location filename="../ui/about_dialog_base.ui" line="203"/>
+        <source>Support us</source>
+        <translation>Поддержите нас</translation>
     </message>
 </context>
 <context>
     <name>AttributeMismatchMessageBox</name>
     <message>
-        <location filename="../osminforesults.py" line="61"/>
+        <location filename="../osminforesults.py" line="75"/>
         <source>Attribute mismatch</source>
         <translation>Несоответствие атрибутов</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="62"/>
+        <location filename="../osminforesults.py" line="76"/>
         <source>Add missing attributes?</source>
         <translation>Добавить отсутствующие атрибуты?</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="63"/>
+        <location filename="../osminforesults.py" line="77"/>
         <source>The feature you are trying to add has attributes that are not present in the target layer.</source>
         <translation>Объект, который вы пытаетесь добавить, содержит атрибуты, отсутствующие в целевом слое.</translation>
     </message>
@@ -71,11 +132,6 @@
         <translation>О OSMInfo</translation>
     </message>
     <message>
-        <location filename="ui/aboutdialogbase.ui" line="44"/>
-        <source>TextLabel</source>
-        <translation type="obsolete">TextLabel</translation>
-    </message>
-    <message>
         <location filename="../ui/settingsdialogbase.ui" line="63"/>
         <source>logo</source>
         <translation>лого</translation>
@@ -84,24 +140,6 @@
         <location filename="../ui/settingsdialogbase.ui" line="83"/>
         <source>OSMInfo</source>
         <translation>OSMInfo</translation>
-    </message>
-    <message>
-        <location filename="ui/aboutdialogbase.ui" line="47"/>
-        <source>version</source>
-        <translation type="obsolete">версия</translation>
-    </message>
-    <message>
-        <location filename="ui/aboutdialogbase.ui" line="70"/>
-        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
-&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
-p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="obsolete">&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
-&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
-p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:'Ubuntu'; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'Sans Serif'; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../ui/settingsdialogbase.ui" line="51"/>
@@ -117,66 +155,37 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>OsmInfo</name>
     <message>
-        <location filename="../osminfo.py" line="74"/>
-        <source>Error</source>
-        <translation>Ошибка</translation>
-    </message>
-    <message>
-        <location filename="../osminfo.py" line="74"/>
-        <source>QGIS %s detected.
-</source>
-        <translation>Обнаружен QGIS %s.
-</translation>
-    </message>
-    <message>
-        <location filename="../osminfo.py" line="74"/>
-        <source>This version of OSMInfo requires at least QGIS version 2.0.
-Plugin will not be enabled.</source>
-        <translation>Эта версия OSMInfo требует QGIS минимум версии 2.0.
-Плагин работать не будет.</translation>
-    </message>
-    <message>
         <location filename="../osminfo.py" line="81"/>
         <source>Get OSM info for a point</source>
         <translation>Получить информацию из OSM</translation>
     </message>
     <message>
-        <location filename="../osminfo.py" line="84"/>
+        <location filename="../osminfo.py" line="86"/>
         <source>Select point</source>
         <translation>Выберите точку</translation>
     </message>
     <message>
-        <location filename="../osminfo.py" line="85"/>
+        <location filename="../osminfo.py" line="87"/>
         <source>Select point to get OpenStreetMap data for</source>
         <translation>Щелкните в точке по которой хотите получить информацию из OpenStreetMap</translation>
     </message>
     <message>
-        <location filename="osminfo.py" line="88"/>
-        <source>About OSMInfo</source>
-        <translation type="obsolete">О OSMInfo</translation>
-    </message>
-    <message>
-        <location filename="../osminfo.py" line="116"/>
+        <location filename="../osminfo.py" line="124"/>
         <source>OSMInfo</source>
         <translation>OSMInfo</translation>
     </message>
     <message>
-        <location filename="../osminfo.py" line="89"/>
+        <location filename="../osminfo.py" line="93"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="../osminfo.py" line="90"/>
+        <location filename="../osminfo.py" line="96"/>
         <source>Set various parameters related to OSMInfo</source>
         <translation>Настройка параметров модуля</translation>
     </message>
     <message>
-        <location filename="../osminfo.py" line="87"/>
-        <source>About</source>
-        <translation type="obsolete">О модуле...</translation>
-    </message>
-    <message>
-        <location filename="../osminfo.py" line="87"/>
+        <location filename="../osminfo.py" line="91"/>
         <source>About…</source>
         <translation>О модуле…</translation>
     </message>
@@ -184,67 +193,57 @@ Plugin will not be enabled.</source>
 <context>
     <name>ResultsDialog</name>
     <message>
-        <location filename="../osminforesults.py" line="96"/>
+        <location filename="../osminforesults.py" line="114"/>
         <source>Feature/Key</source>
         <translation>Объекты/Ключ</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="96"/>
+        <location filename="../osminforesults.py" line="114"/>
         <source>Value</source>
         <translation>Значение</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="170"/>
-        <source>Loading....</source>
-        <translation type="obsolete">Загрузка...</translation>
-    </message>
-    <message>
-        <location filename="../osminforesults.py" line="411"/>
+        <location filename="../osminforesults.py" line="472"/>
         <source>Nearby features</source>
         <translation>Ближайшие объекты</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="444"/>
+        <location filename="../osminforesults.py" line="505"/>
         <source>Is inside</source>
         <translation>Входит в</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="124"/>
+        <location filename="../osminforesults.py" line="152"/>
         <source>Zoom to feature</source>
         <translation>Приблизить</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="96"/>
-        <source>Zoom to selected item</source>
-        <translation type="obsolete">Приблизить к объекту</translation>
-    </message>
-    <message>
-        <location filename="../osminforesults.py" line="477"/>
+        <location filename="../osminforesults.py" line="541"/>
         <source>OSMInfo</source>
         <translation>OSMInfo</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="132"/>
+        <location filename="../osminforesults.py" line="160"/>
         <source>Save feature in new temporary layer</source>
         <translation>Сохранить объект в новый временный слой</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="142"/>
+        <location filename="../osminforesults.py" line="170"/>
         <source>Save feature in selected layer</source>
         <translation>Сохранить объект в выбранный слой</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="155"/>
+        <location filename="../osminforesults.py" line="183"/>
         <source>Copy feature to clipboard</source>
         <translation>Копировать объект в буфер обмена</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="387"/>
+        <location filename="../osminforesults.py" line="448"/>
         <source>Loading...</source>
         <translation>Загрузка...</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="477"/>
+        <location filename="../osminforesults.py" line="541"/>
         <source>Element process error: {e}. Element: {element}.</source>
         <translation>Ошибка обработки элемента: {e}. Элемент: {element}.</translation>
     </message>
@@ -264,28 +263,28 @@ Plugin will not be enabled.</source>
     <message>
         <location filename="../settingsdialog.py" line="58"/>
         <source>Timeout</source>
-        <translation></translation>
+        <translation>Таймаут</translation>
     </message>
 </context>
 <context>
     <name>Worker</name>
     <message>
-        <location filename="../osminfo_worker.py" line="60"/>
+        <location filename="../osminfo_worker.py" line="59"/>
         <source>Worker: %s, %s are wrong coords!</source>
         <translation>Worker: неправильные координаты %s, %s!</translation>
     </message>
     <message>
-        <location filename="../osminfo_worker.py" line="54"/>
+        <location filename="../osminfo_worker.py" line="53"/>
         <source>OSMInfo</source>
         <translation>OSMInfo</translation>
     </message>
     <message>
-        <location filename="../osminfo_worker.py" line="117"/>
+        <location filename="../osminfo_worker.py" line="120"/>
         <source>Error getting data from the server</source>
         <translation>Ошибка получения данных с сервера</translation>
     </message>
     <message>
-        <location filename="../osminfo_worker.py" line="123"/>
+        <location filename="../osminfo_worker.py" line="126"/>
         <source>Error parsing data</source>
         <translation>Ошибка разбора данных</translation>
     </message>
