1e802a832154450337da4cf451e66a8ea5dcdf9f
Ivan Barsukov <i.barsukov@protonmail.com>
2024-11-13 21:00:37 +0700

    Updated translations

--- src/osminfo/i18n/osminfo_ru.ts
+++ src/osminfo/i18n/osminfo_ru.ts
@@ -4,52 +4,52 @@
 <context>
     <name>AboutDialog</name>
     <message>
-        <location filename="../about_dialog.py" line="52"/>
+        <location filename="../about_dialog.py" line="46"/>
         <source> by NextGIS</source>
         <translation> от NextGIS</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="170"/>
+        <location filename="../about_dialog.py" line="181"/>
         <source>REPORT_END</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="175"/>
+        <location filename="../about_dialog.py" line="186"/>
         <source>Developers</source>
         <translation>Разработчики</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="176"/>
+        <location filename="../about_dialog.py" line="187"/>
         <source>Homepage</source>
         <translation>Домашняя страница</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="177"/>
+        <location filename="../about_dialog.py" line="188"/>
         <source>Please report bugs at</source>
         <translation>Пожалуйста, сообщайте о багах в</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="179"/>
+        <location filename="../about_dialog.py" line="190"/>
         <source>bugtracker</source>
         <translation>багтрекер</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="180"/>
+        <location filename="../about_dialog.py" line="191"/>
         <source>Video with an overview of the plugin</source>
         <translation>Видео с обзором на плагин</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="181"/>
+        <location filename="../about_dialog.py" line="192"/>
         <source>Other helpful services by NextGIS</source>
         <translation>Другие полезные сервисы от NextGIS</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="182"/>
+        <location filename="../about_dialog.py" line="193"/>
         <source>Convenient up-to-date data extracts for any place in the world</source>
         <translation>Удобная выборка актуальных данных из любой точки мира</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="185"/>
+        <location filename="../about_dialog.py" line="196"/>
         <source>Fully featured Web GIS service</source>
         <translation>Полнофункциональный Веб ГИС-сервис</translation>
     </message>
@@ -102,190 +102,228 @@ p, li { white-space: pre-wrap; }
     </message>
     <message>
         <location filename="../ui/about_dialog_base.ui" line="203"/>
-        <source>Support us</source>
-        <translation>Поддержите нас</translation>
+        <source>Get involved</source>
+        <translation>Присоединяйтесь</translation>
     </message>
 </context>
 <context>
     <name>AttributeMismatchMessageBox</name>
     <message>
-        <location filename="../osminforesults.py" line="75"/>
+        <location filename="../osminforesults.py" line="100"/>
         <source>Attribute mismatch</source>
         <translation>Несоответствие атрибутов</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="76"/>
+        <location filename="../osminforesults.py" line="101"/>
         <source>Add missing attributes?</source>
         <translation>Добавить отсутствующие атрибуты?</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="77"/>
+        <location filename="../osminforesults.py" line="102"/>
         <source>The feature you are trying to add has attributes that are not present in the target layer.</source>
         <translation>Объект, который вы пытаетесь добавить, содержит атрибуты, отсутствующие в целевом слое.</translation>
     </message>
 </context>
 <context>
-    <name>Dialog</name>
-    <message>
-        <location filename="../ui/settingsdialogbase.ui" line="19"/>
-        <source>About OSMInfo</source>
-        <translation>О OSMInfo</translation>
-    </message>
-    <message>
-        <location filename="../ui/settingsdialogbase.ui" line="63"/>
-        <source>logo</source>
-        <translation>лого</translation>
-    </message>
-    <message>
-        <location filename="../ui/settingsdialogbase.ui" line="83"/>
-        <source>OSMInfo</source>
-        <translation>OSMInfo</translation>
-    </message>
-    <message>
-        <location filename="../ui/settingsdialogbase.ui" line="51"/>
-        <source>distance</source>
-        <translation>Расстояние</translation>
-    </message>
-    <message>
-        <location filename="../ui/settingsdialogbase.ui" line="108"/>
-        <source>timeout</source>
-        <translation></translation>
-    </message>
-</context>
-<context>
     <name>OsmInfo</name>
     <message>
-        <location filename="../osminfo.py" line="81"/>
+        <location filename="../osminfo.py" line="83"/>
         <source>Get OSM info for a point</source>
         <translation>Получить информацию из OSM</translation>
     </message>
     <message>
-        <location filename="../osminfo.py" line="86"/>
+        <location filename="../osminfo.py" line="89"/>
         <source>Select point</source>
         <translation>Выберите точку</translation>
     </message>
     <message>
-        <location filename="../osminfo.py" line="87"/>
+        <location filename="../osminfo.py" line="90"/>
         <source>Select point to get OpenStreetMap data for</source>
         <translation>Щелкните в точке по которой хотите получить информацию из OpenStreetMap</translation>
     </message>
     <message>
-        <location filename="../osminfo.py" line="124"/>
+        <location filename="../osminfo.py" line="147"/>
         <source>OSMInfo</source>
         <translation>OSMInfo</translation>
     </message>
     <message>
-        <location filename="../osminfo.py" line="93"/>
+        <location filename="../osminfo.py" line="100"/>
         <source>Settings</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="../osminfo.py" line="96"/>
+        <location filename="../osminfo.py" line="105"/>
         <source>Set various parameters related to OSMInfo</source>
         <translation>Настройка параметров модуля</translation>
     </message>
     <message>
-        <location filename="../osminfo.py" line="91"/>
-        <source>About…</source>
+        <location filename="../osminfo.py" line="94"/>
+        <source>About plugin…</source>
         <translation>О модуле…</translation>
     </message>
 </context>
 <context>
-    <name>ResultsDialog</name>
+    <name>OsmInfoOptionsErrorPageWidget</name>
     <message>
-        <location filename="../osminforesults.py" line="114"/>
+        <location filename="../settings/osm_info_settings_page.py" line="144"/>
+        <source>Settings dialog was crashed</source>
+        <translation>В диалоге настроек возникла ошибка</translation>
+    </message>
+</context>
+<context>
+    <name>OsmInfoOptionsPageWidget</name>
+    <message>
+        <location filename="../settings/osm_info_settings_page.py" line="84"/>
+        <source>An error occured while settings UI loading</source>
+        <translation>Возникла ошибка при загрузке интерфейса настроек</translation>
+    </message>
+    <message>
+        <location filename="../settings/osm_info_settings_page.py" line="88"/>
+        <source>An error occured in settings UI</source>
+        <translation>Возникла ошибка в интерфейсе настроек</translation>
+    </message>
+</context>
+<context>
+    <name>OsmInfoResultsDock</name>
+    <message>
+        <location filename="../osminforesults.py" line="148"/>
         <source>Feature/Key</source>
         <translation>Объекты/Ключ</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="114"/>
+        <location filename="../osminforesults.py" line="148"/>
         <source>Value</source>
         <translation>Значение</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="472"/>
-        <source>Nearby features</source>
-        <translation>Ближайшие объекты</translation>
+        <location filename="../osminforesults.py" line="191"/>
+        <source>&lt;a href=&quot;https://data.nextgis.com/?{utm}&quot;&gt;Download geodata&lt;/a&gt; for your project</source>
+        <translation>&lt;a href=&quot;https://data.nextgis.com/?{utm}&quot;&gt;Скачайте геоданные&lt;/a&gt; для проекта</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="505"/>
-        <source>Is inside</source>
-        <translation>Входит в</translation>
+        <location filename="../osminforesults.py" line="194"/>
+        <source>&lt;a href=&quot;https://data.nextgis.com/?{utm}&quot;&gt;Fresh geodata&lt;/a&gt; for your project &lt;b&gt;(50% off!)&lt;/b&gt;</source>
+        <translation>&lt;a href=&quot;https://data.nextgis.com/?{utm}&quot;&gt;Свежие геоданные&lt;/a&gt; для проекта. &lt;b&gt;Экономия 50%!&lt;/b&gt;</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="152"/>
+        <location filename="../osminforesults.py" line="229"/>
         <source>Zoom to feature</source>
-        <translation>Приблизить</translation>
+        <translation>Приблизить к объекту</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="541"/>
-        <source>OSMInfo</source>
-        <translation>OSMInfo</translation>
-    </message>
-    <message>
-        <location filename="../osminforesults.py" line="160"/>
-        <source>Save feature in new temporary layer</source>
-        <translation>Сохранить объект в новый временный слой</translation>
-    </message>
-    <message>
-        <location filename="../osminforesults.py" line="170"/>
-        <source>Save feature in selected layer</source>
-        <translation>Сохранить объект в выбранный слой</translation>
-    </message>
-    <message>
-        <location filename="../osminforesults.py" line="183"/>
+        <location filename="../osminforesults.py" line="237"/>
         <source>Copy feature to clipboard</source>
         <translation>Копировать объект в буфер обмена</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="448"/>
+        <location filename="../osminforesults.py" line="245"/>
+        <source>Save feature in new temporary layer</source>
+        <translation>Сохранить объект в новый временный слой</translation>
+    </message>
+    <message>
+        <location filename="../osminforesults.py" line="255"/>
+        <source>Save feature in selected layer</source>
+        <translation>Сохранить объект в выбранный слой</translation>
+    </message>
+    <message>
+        <location filename="../osminforesults.py" line="268"/>
+        <source>Open in OpenStreetMap</source>
+        <translation>Открыть в OpenStreetMap</translation>
+    </message>
+    <message>
+        <location filename="../osminforesults.py" line="276"/>
+        <source>Copy OpenStreetMap URL</source>
+        <translation>Копировать OpenStreetMap URL</translation>
+    </message>
+    <message>
+        <location filename="../osminforesults.py" line="543"/>
         <source>Loading...</source>
         <translation>Загрузка...</translation>
     </message>
     <message>
-        <location filename="../osminforesults.py" line="541"/>
+        <location filename="../osminforesults.py" line="570"/>
+        <source>No features found</source>
+        <translation>Не найдены объекты</translation>
+    </message>
+    <message>
+        <location filename="../osminforesults.py" line="576"/>
+        <source>Nearby features</source>
+        <translation>Ближайшие объекты</translation>
+    </message>
+    <message>
+        <location filename="../osminforesults.py" line="650"/>
         <source>Element process error: {e}. Element: {element}.</source>
         <translation>Ошибка обработки элемента: {e}. Элемент: {element}.</translation>
     </message>
-</context>
-<context>
-    <name>SettingsDialog</name>
     <message>
-        <location filename="../settingsdialog.py" line="56"/>
-        <source>OSMInfo Settings</source>
-        <translation>Настройки OSMInfo</translation>
+        <location filename="../osminforesults.py" line="650"/>
+        <source>OSMInfo</source>
+        <translation>OSMInfo</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.py" line="57"/>
+        <location filename="../osminforesults.py" line="612"/>
+        <source>Is inside</source>
+        <translation>Входит в</translation>
+    </message>
+</context>
+<context>
+    <name>OsmInfoSettingsPageBase</name>
+    <message>
+        <location filename="../ui/osm_info_settings_page_base.ui" line="43"/>
+        <source>Query settings</source>
+        <translation>Настройки запроса</translation>
+    </message>
+    <message>
+        <location filename="../ui/osm_info_settings_page_base.ui" line="52"/>
+        <source>Fetch nearby features</source>
+        <translation>Получать ближайшие объекты</translation>
+    </message>
+    <message>
+        <location filename="../ui/osm_info_settings_page_base.ui" line="65"/>
+        <source>Fetch surrounding features</source>
+        <translation>Получать окружающие объекты</translation>
+    </message>
+    <message>
+        <location filename="../ui/osm_info_settings_page_base.ui" line="77"/>
+        <source>Timeout</source>
+        <translation>Таймаут</translation>
+    </message>
+    <message>
+        <location filename="../ui/osm_info_settings_page_base.ui" line="90"/>
         <source>Distance</source>
         <translation>Расстояние</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.py" line="58"/>
-        <source>Timeout</source>
-        <translation>Таймаут</translation>
+        <location filename="../ui/osm_info_settings_page_base.ui" line="102"/>
+        <source>Other</source>
+        <translation>Прочие</translation>
+    </message>
+    <message>
+        <location filename="../ui/osm_info_settings_page_base.ui" line="108"/>
+        <source>Enable debug messages</source>
+        <translation>Включить отладочные сообщения</translation>
+    </message>
+    <message>
+        <location filename="../ui/osm_info_settings_page_base.ui" line="20"/>
+        <source>Overpass server</source>
+        <translation>Сервер Overpass</translation>
     </message>
 </context>
 <context>
     <name>Worker</name>
     <message>
-        <location filename="../osminfo_worker.py" line="59"/>
+        <location filename="../osminfo_worker.py" line="55"/>
         <source>Worker: %s, %s are wrong coords!</source>
         <translation>Worker: неправильные координаты %s, %s!</translation>
     </message>
     <message>
-        <location filename="../osminfo_worker.py" line="53"/>
-        <source>OSMInfo</source>
-        <translation>OSMInfo</translation>
-    </message>
-    <message>
-        <location filename="../osminfo_worker.py" line="120"/>
+        <location filename="../osminfo_worker.py" line="132"/>
         <source>Error getting data from the server</source>
         <translation>Ошибка получения данных с сервера</translation>
     </message>
     <message>
-        <location filename="../osminfo_worker.py" line="126"/>
-        <source>Error parsing data</source>
+        <location filename="../osminfo_worker.py" line="138"/>
+        <source>Parsing data error</source>
         <translation>Ошибка разбора данных</translation>
     </message>
 </context>
